0e21cdc8a377fc7e05ef813cda1ab419e682c2cd
Adam H <herrmann.adamm@gmail.com>
2026-01-11 00:08:17 -0700

    Learning Activity - C# Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter a number:");
+        string valuefromuser = Console.ReadLine();
+
+        int x = int.Parse(valuefromuser);
+        int y = 2;
+        int z = 5;
+
+
+        if (!(x < y || x < z) && y < z)
+        {
+            Console.WriteLine("x is greater than y");
+        }
+        else if (x == y)
+        {
+            Console.WriteLine("x is Less than y");
+        }
+        else
+        {
+            Console.WriteLine("x is equal to y");
+        }
+
     }
+
 }
