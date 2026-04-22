a47a054fdf60fdfd995fd232353c31ab8cc87dbe
Adam H <herrmann.adamm@gmail.com>
2026-04-21 22:43:57 -0600

    ...

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        foreach (string color in colors)
+        {
+            Console.WriteLine(color);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+
+        string response;
+
+        do. 
+        {
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
+
         Console.WriteLine("Hello Prep3 World!");
+        
+
+
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,38 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+
+@startuml
+Class Journal {
+-_entries: list<Entry>
++Write(): void
++Display(): void
++Save(): void
++Load(): void
+}
+Class Entry {
+-_filename: string
+-_prompt: int
+-_date: string
+-_content: string
++Display(): string
+}
+Class PromptGenerator {
+-_prompts: List<String>
++GeneratePrompt(): int
++Display(int): string
+}
+Entry --> Journal
+Journal --> Entry
+Journal ..> PromptGenerator
+Entry --> PromptGenerator
+@enduml
+
+    
     {
         Console.WriteLine("Hello Develop02 World!");
     }
 }
+
+
+
+
